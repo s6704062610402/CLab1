0a2853bfe14c4177499a5be52dc105b98a197f19
s6704062610402 <s6704062610402@email.kmutnb.ac.th>
2024-11-28 15:01:26 +0700

    Update lab1minx.c

--- lab1minx.c
+++ lab1minx.c
@@ -1,13 +1,19 @@
 #include <stdio.h>
 
 int main() {
-	int n1=2;
-	int n2=5;
+	int n1;
+	int n2;
+	printf("Enter your number1: \n");
+	scanf("%d", &n1);
+	printf("Enter your number2: \n");
+	scanf("%d", &n2);
 	if (n1 > n2) {
-  	printf("n1 is greater than n2");
+		int sum1 = n1 - n2;
+  		printf("num %d is greater than %d is %d ", n1 , n2 , sum1);
 	}
-	if (n2 > n1) {
-  	printf("n2 is greater than n1");
+	else {
+		int sum2 = n2 - n1;
+  		printf("num %d is greater than %d is %d ", n2 , n1 , sum2);
 	}
 	return 0;
 }
